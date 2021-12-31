061fe7c21611c1bd2af28470adfe8e5d57cdaf4c
abg3000 <abg3000@naver.com>
2021-12-31 22:16:33 +0900

    Naver Map call success

---
+++ frontend/src/components/DefaultIntro.js
@@ -0,0 +1,12 @@
+const DefaultIntro = () => {
+  return (
+    <div className="default-intro-container">
+      <div className="default-intro-title">
+        <h1>DEFAULT PAGE</h1>
+      </div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default DefaultIntro;
