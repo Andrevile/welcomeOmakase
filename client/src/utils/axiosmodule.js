c60b91c3273b1d9320809722e64f770ef973e895
abg3000 <abg3000@naver.com>
2022-03-11 22:55:02 +0900

    [Refact] axios 클래스 모듈화, 프록시 방식 -> ACCESS-ALLOW 헤더 추가

--- client/src/utils/axiosmodule.js
+++ client/src/utils/axiosmodule.js
@@ -1,45 +1,65 @@
 import axios from 'axios';
 
-export const Axios = async (Url, Method, Data) => {
-  let option = {};
-  switch (Method) {
-    case 'GET':
-      option = {
-        url: Url,
-      };
-      break;
-    case 'POST':
-      option = {
-        url: Url,
-        method: Method,
-        data: {
-          ...Data,
-        },
-      };
-      break;
-    case 'PUT':
-      option = {
-        url: Url,
-        method: Method,
-        data: {
-          ...Data,
-        },
-      };
-      break;
-    case 'DELETE':
-      option = {
-        url: Url,
-        method: Method,
-      };
-      break;
-    default:
-      break;
+// axios.defaults.baseURL = 'http://localhost:5000';
+
+class api {
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'http://localhost:5000',
+    });
   }
-  try {
-    let response = await axios(option);
-    // response = await response.json();
+  async get(url) {
+    const response = await this.instance.get(url);
     return response.data;
-  } catch (err) {
-    console.log('Axios module call err:', err);
   }
-};
+  async post(url, data) {
+    const response = await this.instance.post(url, data);
+    return response.data;
+  }
+}
+
+export default new api();
+
+// export const Axios = async (Url, Method, Data) => {
+//   let option = {};
+//   switch (Method) {
+//     case 'GET':
+//       option = {
+//         url: Url,
+//       };
+//       break;
+//     case 'POST':
+//       option = {
+//         url: Url,
+//         method: Method,
+//         data: {
+//           ...Data,
+//         },
+//       };
+//       break;
+//     case 'PUT':
+//       option = {
+//         url: Url,
+//         method: Method,
+//         data: {
+//           ...Data,
+//         },
+//       };
+//       break;
+//     case 'DELETE':
+//       option = {
+//         url: Url,
+//         method: Method,
+//       };
+//       break;
+//     default:
+//       break;
+//   }
+//   try {
+//     let response = await axios(option);
+//     // response = await response.json();
+//     return response.data;
+//   } catch (err) {
+//     console.log('Axios module call err:', err);
+//   }
+// };
